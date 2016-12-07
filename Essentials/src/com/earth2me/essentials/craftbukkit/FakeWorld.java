1e2564bb7722fd282f6d0c17352ca9467f763412
dordsor21 <dordsor21@gmail.com>
2016-12-07 22:12:50 +0000

    no message

--- Essentials/src/com/earth2me/essentials/craftbukkit/FakeWorld.java
+++ Essentials/src/com/earth2me/essentials/craftbukkit/FakeWorld.java
@@ -334,11 +334,6 @@ public class FakeWorld implements World {
     }
 
     @Override
-    public <T extends Entity> T spawn(Location lctn, Class<T> type) throws IllegalArgumentException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
     public FallingBlock spawnFallingBlock(Location location, MaterialData materialData) throws IllegalArgumentException {
         throw new UnsupportedOperationException("Not supported yet.");
     }
