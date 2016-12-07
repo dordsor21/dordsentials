bb4cbb2855723df2c8f85b6b9fc685c79b1a5203
dordsor21 <dordsor21@gmail.com>
2016-12-07 22:29:48 +0000

    no message

--- Essentials/src/com/earth2me/essentials/craftbukkit/FakeWorld.java
+++ Essentials/src/com/earth2me/essentials/craftbukkit/FakeWorld.java
@@ -19,7 +19,7 @@ import java.util.Set;
 import java.util.UUID;
 
 
-public abstract class FakeWorld implements World {
+public class FakeWorld implements World {
     private final String name;
     private final Environment env;
 
